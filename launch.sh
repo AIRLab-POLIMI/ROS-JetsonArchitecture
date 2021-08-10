1c547a2f5b12a9c6150bffb0ec207b8a41634a15
Cesare Consonni <37812489+CesareConsonni@users.noreply.github.com>
2021-08-10 17:03:16 +0200

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+source launchers/launch_FakeSubscriber.sh &
+source launchers/launch_Simulator.sh &
+source launchers/launch_ScriptGenExample.sh &
+source launchers/launch_DanceGenExample.sh &
+source launchers/launch_WorldModel.sh
